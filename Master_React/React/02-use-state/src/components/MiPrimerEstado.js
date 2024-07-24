de522fc83efa249d0585cb02ac8c2031d665e283
zono32 <147318166+zono32@users.noreply.github.com>
2024-07-24 10:36:32 +0200

    React Estado use-state

--- Master_React/React/02-use-state/src/components/MiPrimerEstado.js
+++ Master_React/React/02-use-state/src/components/MiPrimerEstado.js
@@ -1,31 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const MiPrimerEstado = () => {
-
-    /*
+  /*
     let nombre = "Master Zono ";
 
     const cambiarNombre = e => {
         nombre = "Kinso Lafuente ";
     }
     */
-    
-    const [nombre, setNombre] = useState("Master zono");
 
-    const cambiarNombre = (e) => {
-        setNombre("Kinso Lafuente");
-    }
+  const [nombre, setNombre] = useState("Master zono");
+
+  const cambiarNombre = (e, nombre) => {
+    setNombre(nombre);
+    console.log(e.target)
+  };
 
   return (
     <div>
-        <h3>Componente: MiPrimerEstado</h3>
-          <strong>
-              {nombre}
-          </strong>
-          &nbsp;
-        <button onClick={cambiarNombre}>cambiar nombre por Kinso Lafuente</button>
+      <h3>Componente: MiPrimerEstado</h3>
+      <strong className="label">{nombre}</strong>
+      &nbsp;
+      <button onClick={(e) => cambiarNombre(e, "Kinso Lafuente")}>
+        cambiar nombre por Kinso Lafuente
+      </button>
+      &nbsp;
+      <input type="text" onChange={e => cambiarNombre(e, e.target.value)} placeholder="cambia el nombre" ></input>
     </div>
-  )
-}
+  );
+};
 
-export default MiPrimerEstado
+export default MiPrimerEstado;
